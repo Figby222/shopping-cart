e05ae66007178524fa40a7c0a1f2bb9bf2b2518c
Figby222 <figby222@outlook.com>
2024-07-05 08:06:02 -0700

    Add routes for Homepage, Store, & ShoppingCart

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,24 @@
 import App from './App';
+import Homepage from "./components/Homepage/Homepage.jsx";
+import Store from "./components/Store/Store.jsx";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart.jsx";
 
 const routes = [
     {
         path: "/",
         element: <App />,
+    },
+    {
+        path: "homepage",
+        element: <Homepage />,
+    },
+    {
+        path: "store:item",
+        element: <Store />,
+    },
+    {
+        path: "shopping-cart",
+        element: <ShoppingCart />,
     },
 ];
 
