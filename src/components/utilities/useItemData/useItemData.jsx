a60f0b540c4927d8121d43c1f0ce1aba379d07ac
Figby222 <figby222@outlook.com>
2024-07-11 16:53:31 -0700

    Add data state & useEffect which fetches from API

--- src/components/utilities/useItemData/useItemData.jsx
+++ src/components/utilities/useItemData/useItemData.jsx
@@ -1,17 +1,30 @@
-const useItemData = function(URL) {
+import React from 'react';
+import { useState, useEffect } from 'react';
+const useItemData = function(URL, fetch) {
     let error;
+    const [ data, setData ] = useState(null);
     if (!URL) {
         error = true;
     }
 
-    const data = {
-        id: 5,
-        title: "a",
-        price: 2,
-        description: "a",
-        imageSrc: "a"
-    }
-    return { error, isLoading: false, data };
+    useEffect(() => {
+        const dataFetch = async () => {
+            const response = await fetch(URL, { mode: "cors" })
+            .then(response => response.json())
+            .catch(error => console.log(error));
+            setData({
+                id: response.id,
+                title: response.title,
+                price: response.price,
+                description: response.description,
+                imageSrc: response.image
+            })
+        }
+
+        dataFetch();
+    }, [ URL, setData, fetch ])
+    
+    return { error, isLoading: false, data: data };
 }
 
 export default useItemData;
