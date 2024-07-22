867964bfd6b643212a258aa4f8c146748a5de6b1
Figby222 <figby222@outlook.com>
2024-07-21 18:43:01 -0700

    Add removeFromCartHandler() prop

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -2,7 +2,7 @@ import "./Store.css";
 import ShoppingCart from "../ShoppingCart/ShoppingCart.jsx";
 import PropTypes from "prop-types";
 
-function Store({ cart, addToCartHandler }) {
+function Store({ cart, addToCartHandler, removeFromCartHandler }) {
     return (
         <>
             <h2>Store</h2>
@@ -16,6 +16,7 @@ function Store({ cart, addToCartHandler }) {
 Store.propTypes = {
     cart: PropTypes.object.isRequired,
     addToCartHandler: PropTypes.func.isRequired,
+    removeFromCartHandler: PropTypes.func.isRequired,
 }
 
 export default Store;
