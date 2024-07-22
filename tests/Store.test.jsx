a7ebe673f29a016c1d649aa913660ebd00ae1bfd
Figby222 <figby222@outlook.com>
2024-07-21 18:46:25 -0700

    Populate props in tests with dummy values
    Populate props in Store rendering suite with dummy
    values

--- tests/Store.test.jsx
+++ tests/Store.test.jsx
@@ -4,20 +4,32 @@ import { describe, it, expect } from "vitest";
 
 describe("Store rendering", () => {
     it("renders heading with text \"Store\"", () => {
-        render(<Store />);
+        render(
+            <Store cart={{}} 
+            addToCartHandler={() => {}} 
+            removeFromCartHandler={() => {}} />
+        );
         expect(screen.getByRole("heading", { name: "Store" }))
             .toBeInTheDocument();
     });
     
     it("renders an shopping-cart-item-count element", () => {
-        render(<Store />);
+        render(
+            <Store cart={{}} 
+            addToCartHandler={() => {}}
+            removeFromCartHandler={() => {}} />
+        );
         expect(screen.getByLabelText("shopping-cart-item-count"))
             .toBeInTheDocument();
     })
 
     it(`renders shopping-cart-item-count element with the text:
          "Item count: <digit>"`, () => {
-        render(<Store />);
+        render(
+            <Store cart={{}} 
+            addToCartHandler={() => {}} 
+            removeFromCartHandler={() => {}}/>
+        );
         const shoppingCartItemCount = 
             screen.getByLabelText("shopping-cart-item-count");
         
