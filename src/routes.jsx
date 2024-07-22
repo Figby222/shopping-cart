5d1362f4b7adb994eed9d6c199200566910b38d1
Figby222 <figby222@outlook.com>
2024-07-21 18:15:55 -0700

    Remove route to Store component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,9 @@
 import App from './App';
-import Store from "./components/Store/Store.jsx";
 
 const routes = [
     {
         path: "/",
         element: <App />,
-    },
-    {
-        path: "store:item",
-        element: <Store />,
     },
 ];
 
