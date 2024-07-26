627999142c330b64a8c35b24dd534466e0c20d39
Figby222 <figby222@outlook.com>
2024-07-26 03:39:02 -0700

    Render cart items as unordered list

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -8,15 +8,18 @@ function ShoppingCart({ cart, removeFromCartHandler }) {
             <p className="shopping-cart-item-count">
                 Item count: {cart.length}
             </p>
-            {cart.map((item) => {
-                return (
-                    <div className="cart-item" aria-label="cart item" key={item.id}>
-                        <p className="item-title">Title: {item.title}</p>
-                        <p className="item-quantity">Quantity: {item.quantity}</p>
-                        <p className="price">Price: {item.price * item.quantity}</p>
-                    </div>
-                )
-            })}
+            <ul className="cart-items">
+                {cart.map((item) => {
+                    return (
+                        <li className="cart-item" aria-label="cart item" key={item.id}>
+                            <p className="item-title">Title: {item.title}</p>
+                            <p className="item-quantity">Quantity: {item.quantity}</p>
+                            <p className="price">Price: {item.price * item.quantity}</p>
+                        </li>
+                    )
+                })}
+
+            </ul>
         </>
     );
 }
