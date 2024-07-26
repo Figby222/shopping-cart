0cbbd0a6e16f7eb649b3f9d0c041d2247c33b24f
Figby222 <figby222@outlook.com>
2024-07-26 05:28:39 -0700

    Add itemPrice prop & paragraph

--- src/components/utilities/CartItem/CartItem.jsx
+++ src/components/utilities/CartItem/CartItem.jsx
@@ -1,10 +1,11 @@
 import './CartItem.css';
 import PropTypes from 'prop-types';
-function CartItem({ id, itemTitle, itemQuantity, removeFromCartHandler }) {
+function CartItem({ id, itemTitle, itemQuantity, itemPrice, removeFromCartHandler }) {
     return (
         <> 
             <p className="item-quantity">Quantity: {itemQuantity}</p>
             <p className="item-title">Title: {itemTitle}</p>
+            <p className="item-price">Price: {itemPrice * itemQuantity}</p>
             <button 
                 type="button" 
                 className="remove-from-cart" 
@@ -21,6 +22,7 @@ CartItem.propTypes = {
     id: PropTypes.number.isRequired,
     itemTitle: PropTypes.string.isRequired,
     itemQuantity: PropTypes.number.isRequired,
+    itemPrice: PropTypes.number.isRequired,
     removeFromCartHandler: PropTypes.func.isRequired
 }
 
