465ab200bf2ec7f900697a7369944f90dd3304f3
Figby222 <figby222@outlook.com>
2024-07-26 05:17:03 -0700

    Raise CartItem tests to Store test file
    Change props respectively
    Remove CartItem test file

--- tests/Store.test.jsx
+++ tests/Store.test.jsx
@@ -1,6 +1,7 @@
 import Store from "../src/components/Store/Store.jsx";
 import { screen, render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+import userEvent from '@testing-library/user-event';
 
 describe("Store rendering", () => {
     it("renders heading with text \"Store\"", () => {
@@ -372,5 +373,214 @@ describe("total price", () => {
 
         const totalParagraph = screen.getByText(/total/i);
         expect(totalParagraph.textContent).toMatch(/100/i);
+    })
+})
+
+describe("CartItem", () => {
+    it("renders itemTitle paragraph", () => {
+        const mockCart = [
+            {
+                id: 1,
+                title: "T-shirt",
+                quantity: 1,
+                price: 20,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        expect(screen.getByText(/title/i)).toBeInTheDocument();
+    })
+
+    it("renders itemTitle paragraph with prop-given text", () => {
+        const mockCart = [
+            {
+                id: 1,
+                title: "T-shirt",
+                quantity: 1,
+                price: 20,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        const itemTitle = screen.getByText(/title/i);
+        expect(itemTitle.textContent).toMatch(/t-shirt/i);
+    })
+
+    it("renders itemTitle paragraph with different prop-given text", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 1,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        const itemTitle = screen.getByText(/title/i);
+        expect(itemTitle.textContent).toMatch(/black pants/i);
+    })
+
+    it("renders quantity paragraph", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 1,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        expect(screen.getByText(/quantity/i)).toBeInTheDocument();
+    })
+
+    it("renders quantity paragraph with prop-given value", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 1,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        const quantity = screen.getByText(/quantity/i); 
+        expect(quantity.textContent).toMatch(/1/i);
+    })
+
+    it("renders quantity paragraph with different prop-given value", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        const quantity = screen.getByText(/quantity/i); 
+        expect(quantity.textContent).toMatch(/2/i);
+    })
+})
+
+describe("remove from cart button", () => {
+    it("renders a button to remove from cart", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        expect(screen.getByRole("button", { name: /remove item from cart/i }))
+            .toBeInTheDocument();
+    })
+
+    it("renders remove from cart button with 'X' textContent", () => {
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={() => {}}
+            addToCartHandler={() => {}} />);
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        expect(removeFromCart.textContent).toMatch(/X/i);
+    })
+
+    it("calls removeFromCartHandler function on click", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={onClick}
+            addToCartHandler={() => {}} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick).toHaveBeenCalled();
+    })
+
+    it("calls removeFromCartHandler function with id arg", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={onClick}
+            addToCartHandler={() => {}} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick.mock.calls[0][0]).toBe(1);
+    })
+
+    it("calls removeFromCartHandler function with different id arg", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<Store 
+            cart={mockCart} 
+            removeFromCartHandler={onClick}
+            addToCartHandler={() => {}} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick.mock.calls[0][0]).toBe(2);
     })
 })
