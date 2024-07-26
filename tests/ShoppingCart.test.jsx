4ecddff95688816006bd881f42f7ce42496aa3f6
Figby222 <figby222@outlook.com>
2024-07-26 06:04:13 -0700

    MV cart handler tests to ShoppingCart test file
    Move removeFromCartHandler mock tests to
    ShoppingCart test file

--- tests/ShoppingCart.test.jsx
+++ tests/ShoppingCart.test.jsx
@@ -1,5 +1,6 @@
 import ShoppingCart from "../src/components/ShoppingCart/ShoppingCart.jsx";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, it, expect, vi } from "vitest";
 
 describe("ShoppingCart", () => {
@@ -55,5 +56,67 @@ describe("ShoppingCart", () => {
 
         expect(itemCountParagraph.textContent)
             .toMatch(/2/i);
+    })
+})
+
+describe("removeFromCartHandler mock tests", () => {
+    it("calls removeFromCartHandler function on click", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<ShoppingCart 
+            cart={mockCart} 
+            removeFromCartHandler={onClick} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick).toHaveBeenCalled();
+    })
+
+    it("calls removeFromCartHandler function with id arg", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 1,
+                title: "T-shirt",
+                quantity: 2,
+                price: 20,
+            }
+        ]
+        render(<ShoppingCart 
+            cart={mockCart} 
+            removeFromCartHandler={onClick} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick.mock.calls[0][0]).toBe(1);
+    })
+
+    it("calls removeFromCartHandler function with different id arg", async () => {
+        const onClick = vi.fn();
+        const mockCart = [
+            {
+                id: 2,
+                title: "Black pants",
+                quantity: 2,
+                price: 40,
+            }
+        ]
+        render(<ShoppingCart 
+            cart={mockCart} 
+            removeFromCartHandler={onClick} />);
+        const user = userEvent.setup();
+
+        const removeFromCart = screen.getByRole("button", { name: /remove item from cart/i });
+        await user.click(removeFromCart);
+        expect(onClick.mock.calls[0][0]).toBe(2);
     })
 })
