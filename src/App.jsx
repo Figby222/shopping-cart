8fa5552335f5b26f69302d3ef2072d6bc0891119
Figby222 <figby222@outlook.com>
2024-08-06 09:00:27 -0700

    Set up routing with params, & add cart state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { useParams } from 'react-router-dom'
+import { useState } from "react";
+import Homepage from "./components/Homepage/Homepage.jsx";
+import Store from './components/Store/Store.jsx';
 
 function App() {
-
+  const { name } = useParams();
+  const [ cart, setCart ] = useState([]);
   return (
     <>
-      
+      {name === "store" ? 
+        <Store cart={cart} setCart={setCart} /> :
+        <Homepage />
+      }
     </>
   )
 }
