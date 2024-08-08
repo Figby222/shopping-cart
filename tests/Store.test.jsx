6b5b690a12f8d7167d88a880849197bf690d563b
Figby222 <figby222@outlook.com>
2024-08-08 05:25:45 -0700

    Assert add to cart doesn't work with NaN quantity
    Add beforeEach for each test which resets mocks
    & assigns globalThis.fetch to a mocked fetch func

--- tests/Store.test.jsx
+++ tests/Store.test.jsx
@@ -7,61 +7,68 @@ import { act } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import routes from "../src/routes.jsx";
 
-globalThis.fetch = vi.fn((URL) => {
-    if (URL === "https://fakestoreapi.com/products/1") {
-        return Promise.resolve({
-            json: () => Promise.resolve({
-                id: 1,
-                title: "Oriental Fresh Shirt",
-                price: 124,
-                description: "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
-                image: "https://loremflickr.com/640/480"
+beforeEach(() => { // maybe remove beforeEach,
+    vi.resetAllMocks();
+    globalThis.fetch = vi.fn((URL) => {
+        if (URL === "https://fakestoreapi.com/products/1") {
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    id: 1,
+                    title: "Oriental Fresh Shirt",
+                    price: 124,
+                    description: "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+                    image: "https://loremflickr.com/640/480"
+                })
             })
-        })
-    }
-
-    if (URL === "https://fakestoreapi.com/products/2") {
-        return Promise.resolve({
-            json: () => Promise.resolve({
-                id: 2,
-                title: "Mens Casual Premium Slim Fit T-Shirts",
-                price: 22.3,
-                description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-                image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+        }
+    
+        if (URL === "https://fakestoreapi.com/products/2") {
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    id: 2,
+                    title: "Mens Casual Premium Slim Fit T-Shirts",
+                    price: 22.3,
+                    description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+                    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+                })
             })
-        })
-
-    }
-
-    if (URL === "https://fakestoreapi.com/products/3") {
-        return Promise.resolve({
-            json: () => Promise.resolve({
-                id: 3,
-                title: "Mens Cotton Jacket",
-                price: 55.99,
-                description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-                    "category": "men's clothing",
-                image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-                
+    
+        }
+    
+        if (URL === "https://fakestoreapi.com/products/3") {
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    id: 3,
+                    title: "Mens Cotton Jacket",
+                    price: 55.99,
+                    description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+                        "category": "men's clothing",
+                    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+                    
+                })
             })
-        })
-    }
-
-    if (URL === "https://fakestoreapi.com/products/4") {
-        return Promise.resolve({
-            json: () => Promise.resolve({
-                id: 4,
-                title: "red shirt",
-                price: 128,
-                description: "A super cool red shirt",
-                image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        }
+    
+        if (URL === "https://fakestoreapi.com/products/4") {
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    id: 4,
+                    title: "red shirt",
+                    price: 128,
+                    description: "A super cool red shirt",
+                    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+                })
             })
+        }
+    
+        return Promise.resolve({
+            status: 404,
         })
-    }
-
-    return Promise.resolve({
-        status: 404,
     })
+})
+
+beforeEach(() => {
+
 })
 
 describe("Store rendering", () => {
@@ -1013,6 +1020,54 @@ describe("add to cart button", () => {
         expect(cartItem1Title.textContent).toMatch(/Oriental fresh shirt/i);
         expect(cartItem1Price.textContent).toMatch(124);
         expect(cartItem1Quantity.textContent).toMatch(/6/i);
+    })
+
+    it("does not work when letter is set as itemQuantity", async () => {
+        const StoreItem = await import("../src/components/utilities/StoreItem/StoreItem.jsx");
+
+        StoreItem.default = vi.fn(({ addToCartHandler }) => {
+            const item = {
+                id: 2,
+                title: "Mens Casual Premium Slim Fit T-Shirts",
+                price: 22.3,
+                description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+                imageSrc: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+            }
+            console.log("A");
+
+            return (
+                <>
+                    <button onClick={() => addToCartHandler(item, "e")}>Add to cart</button>
+                </>
+            )
+        })
+        const MockParent = ({ initialMockCart }) => {
+            const [ cart, setCart ] = useState(initialMockCart);
+            
+            return (
+                <>
+                    <Store cart={cart} setCart={setCart} />
+                </>
+            )
+        }
+
+        await act(async () => {
+            render(<MockParent initialMockCart={[]} />, { wrapper: BrowserRouter });
+        })
+
+        const addToCartButtons = screen.getAllByRole("button", { name: /add to cart/i });
+        const addToCartButton = addToCartButtons[0];
+        
+        expect(addToCartButtons.length).toBeGreaterThan(0);
+
+        const user = userEvent.setup();
+        await user.click(addToCartButton);;
+
+        const removeFromCartButtons = screen.queryAllByLabelText(/remove item from cart/i);
+
+        expect(removeFromCartButtons).toHaveLength(0);
+
+
     })
 })
 
