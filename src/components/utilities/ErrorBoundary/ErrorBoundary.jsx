e59de10ed7373e7293045f8bcfb521def15b6612
Figby222 <figby222@outlook.com>
2024-08-12 07:18:33 -0700

    Add header and main
    Add header and main elements for semantics

--- src/components/utilities/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/utilities/ErrorBoundary/ErrorBoundary.jsx
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 function ErrorBoundary() {
     return(
         <>
-            <h1 className="invalid-page">Invalid page</h1>
-            <p className="main-text">
-                It appears you're lost,
-                <Link to="/homepage">click here to go back home!</Link>
-            </p>
+            <header>
+                <h1 className="invalid-page">Invalid page</h1>
+            </header>
+            <main>
+                <p className="main-text">
+                    It appears you're lost,
+                    <Link to="/homepage">click here to go back home!</Link>
+                </p>
+            </main>
         </>
     );
 }
