7d3f5ca1b624267449a499a70890b3174eb54cc7
Figby222 <figby222@outlook.com>
2024-08-12 07:27:46 -0700

    Add errorElement to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import App from './App';
+import ErrorBoundary from './components/utilities/ErrorBoundary/ErrorBoundary';
 
 const routes = [
     {
         path: "/:name",
         element: <App />,
+        errorElement: <ErrorBoundary />,
     },
 ];
 
