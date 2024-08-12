f9ab724196eba06a44f4e11a5fe7ff07f6e67cfb
Figby222 <figby222@outlook.com>
2024-08-12 06:49:49 -0700

    Add ErrorBoundary component

---
+++ src/components/utilities/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,6 @@
+import "./ErrorBoundary.css";
+function ErrorBoundary() {
+    return(<>blah blah blah</>);
+}
+
+export default ErrorBoundary;
---
+++ tests/ErrorBoundary.test.jsx
@@ -0,0 +1,9 @@
+import ErrorBoundary from "../src/components/utilities/ErrorBoundary/ErrorBoundary.jsx";
+import { screen, render } from "@testing-library/react"
+import { describe, it, expect, vi } from "vitest";
+
+describe("blahblahablhahasikuyvgfdoaliugsf", () => {
+    it("cookies", () => {
+        
+    })
+})
