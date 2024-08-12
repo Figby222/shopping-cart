1d2d7770f6f3798f450b94354738aa4e75535c55
Figby222 <figby222@outlook.com>
2024-08-12 07:21:08 -0700

    Add logic to render ErrorBoundary on invalid URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useState } from "react";
 import Homepage from "./components/Homepage/Homepage.jsx";
 import Store from './components/Store/Store.jsx';
+import ErrorBoundary from './components/utilities/ErrorBoundary/ErrorBoundary.jsx';
 
 function App() {
   const { name } = useParams();
@@ -11,7 +12,9 @@ function App() {
     <>
       {name === "store" ? 
         <Store cart={cart} setCart={setCart} /> :
-        <Homepage />
+      name === "homepage" ?
+        <Homepage /> :
+        <ErrorBoundary />
       }
     </>
   )
