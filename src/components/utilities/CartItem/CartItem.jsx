1af261ec77fbcc81c6c76be16a802eb9cba57173
Figby222 <figby222@outlook.com>
2024-08-13 16:08:58 -0700

    Contain each cart item attribute label in a span
    Add .cart-item-attribute to each cart item
    attribute, & .cart-item-attribute-label to each
    cart item attribute label

--- src/components/utilities/CartItem/CartItem.jsx
+++ src/components/utilities/CartItem/CartItem.jsx
@@ -11,9 +11,15 @@ function CartItem({ item, removeFromCartHandler }) {
             >
             X
             </button>
-            <p className="cart-item-title">Title: {item.title}</p>
-            <p className="cart-item-quantity">Quantity: {item.quantity}</p>
-            <p className="cart-item-price">Price: {item.price * item.quantity}</p>
+            <p className="cart-item-title cart-item-attribute">{item.title}</p>
+            <p className="cart-item-quantity cart-item-attribute">
+                <span className="cart-item-attribute-label">Quantity:</span>
+                {item.quantity}
+            </p>
+            <p className="cart-item-price cart-item-attribute" aria-labelledby="cart-item-price">
+                <span className="cart-item-attribute-label">Price:</span>
+                {item.price * item.quantity}
+            </p>
         </>
     );
 }
