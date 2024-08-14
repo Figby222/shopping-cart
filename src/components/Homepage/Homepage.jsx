4a55e7870d523db2921032553a5ae88514ecd146
Figby222 <figby222@outlook.com>
2024-08-13 20:57:26 -0700

    Add a paragraph for my story

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -16,6 +16,11 @@ function Homepage() {
             </nav>
             <main>
                 <h2 className="story-heading">My Story</h2>
+                <p className="story">
+                    I love cookies a lot. I love cookies so much that I'll
+                    cook them right now. Literally, I'm preheating the oven
+                    at this current moment.
+                </p>
             </main>
         </>
     )
